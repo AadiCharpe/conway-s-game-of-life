9371b93366f8fcf7ad657d5ff5be7309ca4eaf0a
Aadi Charpe <aadi.charpe@hotmail.com>
2025-12-30 18:50:51 -0800

    sizes

--- life.java
+++ life.java
@@ -6,6 +6,7 @@ import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 import javax.swing.JButton;
 import javax.swing.JSlider;
+import javax.swing.JComboBox;
 
 import java.awt.Graphics;
 import java.awt.Graphics2D;
@@ -16,6 +17,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseAdapter;
+
+import java.util.StringTokenizer;
 
 public class life {
     public static void main(String[] args) {
@@ -62,11 +65,22 @@ class lifeFrame extends JFrame {
         });
         panel.setDelay(delay.getValue());
 
+        JComboBox sizes = new JComboBox(new String[] {"10x10", "20x20", "50x50"});
+        sizes.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                start.setText("Start");
+                StringTokenizer tokenizer = new StringTokenizer((String) sizes.getSelectedItem(), "x");
+                panel.reset(Integer.parseInt(tokenizer.nextToken()));
+            }
+        });
+
         JPanel ui = new JPanel();
         ui.add(start);
         ui.add(step);
         ui.add(new JLabel("Speed:"));
         ui.add(delay);
+        ui.add(new JLabel("Size:"));
+        ui.add(sizes);
 
         getContentPane().add(ui, "South");
     }
@@ -76,8 +90,9 @@ class lifePanel extends JPanel {
     private boolean[][] cells;
     private boolean running = false;
     private Timer timer;
+    private int SIZE = 10;
     public lifePanel() {
-        cells  = new boolean[20][20];
+        cells  = new boolean[SIZE][SIZE];
         timer = new Timer(250, new ActionListener() {
             public void actionPerformed(ActionEvent e) {
                 nextGeneration();
@@ -88,7 +103,7 @@ class lifePanel extends JPanel {
         addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
                 if(!running) {
-                    cells[(int) Math.floor(e.getY() / 30)][(int) Math.floor(e.getX() / 30)] = cells[(int) Math.floor(e.getY() / 30)][(int) Math.floor(e.getX() / 30)] ? false : true;
+                    cells[(int) Math.floor(e.getY() / (600 / SIZE))][(int) Math.floor(e.getX() / (600 / SIZE))] = cells[(int) Math.floor(e.getY() / (600 / SIZE))][(int) Math.floor(e.getX() / (600 / SIZE))] ? false : true;
                     repaint();
                 }
             }
@@ -124,16 +139,23 @@ class lifePanel extends JPanel {
                 else
                     color = Color.DARK_GRAY;
                 g2.setPaint(color);
-                g2.fill(new Rectangle2D.Double(j * 30, i * 30, 30, 30));
+                g2.fill(new Rectangle2D.Double(j * (600 / SIZE), i * (600 / SIZE), (600 / SIZE), (600 / SIZE)));
                 
                 g2.setPaint(color.darker());
-                g2.draw(new Rectangle2D.Double(j * 30, i * 30, 30, 30));
+                g2.draw(new Rectangle2D.Double(j * (600 / SIZE), i * (600 / SIZE), (600 / SIZE), (600 / SIZE)));
             }
         }
     }
 
+    public void reset(int size) {
+        SIZE = size;
+        timer.stop();
+        cells = new boolean[SIZE][SIZE];
+        repaint();
+    }
+
     public void nextGeneration() {
-        boolean[][] newGen = new boolean[20][20];
+        boolean[][] newGen = new boolean[SIZE][SIZE];
 
         for (int i = 0; i < cells.length; i++) {
             newGen[i] = cells[i].clone();
@@ -159,20 +181,20 @@ class lifePanel extends JPanel {
         if(i > 0) {
             if(j > 0)
                 if(cells[i - 1][j - 1]) count++;
-            if(j < 19)
+            if(j < SIZE - 1)
                 if(cells[i - 1][j + 1]) count++;
             if(cells[i - 1][j]) count++;
         }
-        if(i < 19) {
+        if(i < SIZE - 1) {
             if(j > 0)
                 if(cells[i + 1][j - 1]) count++;
-            if(j < 19)
+            if(j < SIZE - 1)
                 if(cells[i + 1][j + 1]) count++;
             if(cells[i + 1][j]) count++;
         }
         if(j > 0)
             if(cells[i][j - 1]) count++;
-        if(j < 19)
+        if(j < SIZE - 1)
             if(cells[i][j + 1]) count++;
         return count;
     }
